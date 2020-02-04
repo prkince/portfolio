dcb113bcd5db7e27efa9b359c02d124cb1a1ef05
Pierrick Delage <pierrick.delage@gmail.com>
2020-02-04 01:04:45 +0100

    navbar & home page done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,14 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def bio
+  end
+
+  def projects
+  end
+
+  def contact
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "bio", to: "pages#bio", as: :bio
+  get "projects", to: "pages#projects", as: :projects
+  get "contact", to: "pages#contact", as: :contact
 end
