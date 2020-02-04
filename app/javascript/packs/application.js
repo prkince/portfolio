26cb7ebeba2205d6d1846fdac5cd6b61e04fad09
Pierrick Delage <pierrick.delage@gmail.com>
2020-02-04 20:25:56 +0100

    adding some ajax animation

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -25,4 +25,23 @@ if (location.href == "http://localhost:3000/" || location.href == "https://portf
       easing: "easeOutExpo",
       delay: 1000
     });
+
+
+  const textWrapper2 = document.querySelector('.ml4');
+  textWrapper2.innerHTML = textWrapper2.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+  anime.timeline({loop: true})
+    .add({
+      targets: '.ml4 .letter',
+      opacity: [0,1],
+      easing: "easeOutExpo",
+      duration: 2250,
+      delay: (el, i) => 150 * (i+1)
+    }).add({
+      targets: '.ml4',
+      opacity: 0,
+      duration: 1000,
+      easing: "easeInOutQuad",
+      delay: 1000
+    });
 }
