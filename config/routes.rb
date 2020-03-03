0c4dcbba7ea949bdf479234d4035e6ff313e2d98
Pierrick Delage <pierrick.delage@gmail.com>
2020-03-03 15:46:17 +0100

    adding fr pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "bio", to: "pages#bio", as: :bio
-  get "bio-fr", to: "pages#bio-fr"
-  get "home-fr", to: "pages#home-fr"
+  get "biographie", to: "pages#biographie"
+  get "accueil", to: "pages#accueil"
   get "projects", to: "pages#projects", as: :projects
-  get "projects-fr", to: "pages#projects-fr"
-  get    "contact", to: "contacts#new", as: :contact
-  post   "contacts", to: "contacts#create"
+  get "projets", to: "pages#projets"
+  get  "contact", to: "contacts#new", as: :contact
+  post "contacts", to: "contacts#create"
 end
